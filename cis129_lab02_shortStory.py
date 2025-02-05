e9619854adaadb596a5f04d352f76e0f2d82e156
jmiller130 <jmiller130@mail.pima.edu>
2025-02-05 08:44:37 -0700

    Updated cis129_lab02_shortStory.py
    Story was changed to be about knights

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+# Author: John Miller
+# My first Python lab in my CIS129 course
+# Prints a simple story
+print('Once upon a time lived a knight in shining armor')
+print('It was this knights dream to be a hero one day but he was never given the oppurtunity')
+print('Then one day a dragon came into the city where he lived and started wreaking havoc')
+print(' The dragon was destroying everything house, markets, farms, everything')
+print(' The knight knew he had to do something')
+print(' the knight sprung into action in a fight that lasted for 3 days')
+print(' At the end of that time the knight vanquisted the dragon and finally was recognized as the hero he always wanted to be')
